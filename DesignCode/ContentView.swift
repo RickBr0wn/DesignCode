eb2327162c14030d4cc2a694f9c42b074b470b9e
Rick Brown <ricky.brown.00@gmail.com>
2020-09-07 17:07:56 +0100

    adds dismiss and and anchors

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
   @State var show: Bool = false
   @State var viewState: CGSize = .zero
   @State var showCard: Bool = false
+  @State var bottomState: CGSize = .zero
+  @State var showFull: Bool = false
   
   var body: some View {
     ZStack {
@@ -54,7 +56,7 @@ struct ContentView: View {
       CardView()
         .frame(width: showCard ? 375 : 340, height: 220)
         .background(Color.black)
-//        .cornerRadius(20)
+        //        .cornerRadius(20)
         .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
         .shadow(radius: 20)
         .offset(x: viewState.width, y: viewState.height)
@@ -75,10 +77,36 @@ struct ContentView: View {
         }
       )
       
+//      Text("\(bottomState.height)").offset( y: -300).foregroundColor(.white)
+      
       BottomCardView()
         .offset(x: 0, y: showCard ?  360 : 1000)
+        .offset(y: bottomState.height)
         .blur(radius: show ? 20 : 0)
         .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+        .gesture(
+          DragGesture().onChanged { value in
+            self.bottomState = value.translation
+            if self.showFull {
+              self.bottomState.height += -300
+            }
+            if self.bottomState.height < -300 {
+              self.bottomState.height = -300
+            }
+          }
+          .onEnded { value in
+            if self.bottomState.height > 100 {
+              self.showCard = false
+            }
+            if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+              self.bottomState.height = -300
+              self.showFull = true
+            } else {
+              self.bottomState = .zero
+              self.showFull = false
+            }
+          }
+      )
     }
   }
 }
