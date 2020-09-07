c51ec1995bb7a822be4d80a413cb9032fafdd1fd
Rick Brown <ricky.brown.00@gmail.com>
2020-09-07 21:18:50 +0100

    adds bindings and globally accessed screen size

---
+++ DesignCode/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Rick Brown on 07/09/2020.
+//  Copyright © 2020 Rick Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  @Binding var showProfile: Bool
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Watching")
+          .font(.system(size: 28, weight: .bold))
+        
+        Spacer()
+        
+        AvatarView(showProfile: $showProfile)
+      }
+      .padding(.horizontal)
+      .padding(.top, 30)
+      
+      Spacer()
+    }
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView(showProfile: .constant(false))
+  }
+}
