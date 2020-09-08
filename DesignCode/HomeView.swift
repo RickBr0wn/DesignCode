e452d8d5bd3c1d3b64e45d17ef65e264d25ecb9e
Rick Brown <ricky.brown.00@gmail.com>
2020-09-08 21:07:32 +0100

    adds custom fonts and modifiers

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -17,6 +17,7 @@ struct HomeView: View {
       HStack {
         Text("Watching")
           .font(.system(size: 28, weight: .bold))
+          .modifier(CustomerFontModifier(size: 28))
         
         Spacer()
         
@@ -40,23 +41,11 @@ struct HomeView: View {
       .padding(.horizontal)
       .padding(.top, 30)
       
-      HStack(spacing: 12.0) {
-        RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percentage: 68, show: .constant(true))
-        
-        VStack(alignment: .leading, spacing: 4.0) {
-          Text("6 minutes left")
-            .font(.subheadline)
-            .fontWeight(.bold)
-          
-          Text("Watched 10 mins today")
-            .font(.caption)
-        }
+      ScrollView(.horizontal, showsIndicators: false) {
+        WatchRingsView()
+          .padding(.horizontal, 30)
+          .padding(.bottom, 30)
       }
-      .padding(8)
-      .background(Color.white)
-      .cornerRadius(20)
-      .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-      .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
       
       ScrollView(.horizontal, showsIndicators: false) {
         HStack(spacing: 20) {
@@ -131,3 +120,43 @@ let sectionData = [
   Section(title: "Prototype design in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card5")), color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))),
   Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))),
   Section(title: "Advanced SwiftUI", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background1")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))]
+
+struct WatchRingsView: View {
+  var body: some View {
+    HStack(spacing: 30) {
+      HStack(spacing: 12.0) {
+        RingView(color1: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), width: 44, height: 44, percentage: 68, show: .constant(true))
+        
+        VStack(alignment: .leading, spacing: 4.0) {
+          Text("6 minutes left")
+            .bold()
+            .modifier(FontModifier(style: .subheadline))
+          
+          Text("Watched 10 mins today")
+            .modifier(FontModifier(style: .caption))
+          
+        }
+      }
+      .padding(8)
+      .background(Color.white)
+      .cornerRadius(20)
+      .modifier(ShadowModifier())
+      
+      HStack(spacing: 12.0) {
+        RingView(color1: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), color2: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), width: 32, height: 32, percentage: 53, show: .constant(true))
+      }
+      .padding(8)
+      .background(Color.white)
+      .cornerRadius(20)
+      .modifier(ShadowModifier())
+      
+      HStack(spacing: 12.0) {
+        RingView(color1: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), color2: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), width: 32, height: 32, percentage: 87, show: .constant(true))
+      }
+      .padding(8)
+      .background(Color.white)
+      .cornerRadius(20)
+      .modifier(ShadowModifier())
+    }
+  }
+}
