fde4758fe35788a9031d4dbdc5b916d0a2a82c27
Rick Brown <ricky.brown.00@gmail.com>
2020-09-08 16:28:41 +0100

    adds RingView to HomeView

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -77,9 +77,9 @@ struct ContentView: View {
         }
       )
       
-//      Text("\(bottomState.height)").offset( y: -300).foregroundColor(.white)
+      //      Text("\(bottomState.height)").offset( y: -300).foregroundColor(.white)
       
-      BottomCardView()
+      BottomCardView(show: $showCard)
         .offset(x: 0, y: showCard ?  360 : 1000)
         .offset(y: bottomState.height)
         .blur(radius: show ? 20 : 0)
@@ -172,6 +172,8 @@ struct TitleView: View {
 }
 
 struct BottomCardView: View {
+  @Binding var show: Bool
+  
   var body: some View {
     VStack(spacing: 20) {
       Rectangle()
@@ -183,6 +185,25 @@ struct BottomCardView: View {
         .multilineTextAlignment(.center)
         .font(.subheadline)
         .lineSpacing(4)
+      
+      HStack(spacing: 20.0) {
+        RingView(color1: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), width: 88, height: 88, percentage: 78, show: $show)
+          .animation(Animation.easeInOut.delay(0.3))
+        
+        VStack(alignment: .leading, spacing: 8.0) {
+          Text("SwiftUI")
+            .fontWeight(.bold)
+          
+          Text("12 of 12 sections completed\n10 hours spent so far")
+            .font(.footnote)
+            .foregroundColor(.gray)
+            .lineSpacing(4)
+        }
+        .padding(20)
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+      }
       
       Spacer()
       
